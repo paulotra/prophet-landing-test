7ca14fc908d9431a5bfd0d9699d9e15aed10a041
paulotra <trajanopaulo10@gmail.com>
2021-03-25 14:59:37 +0800

    Add Style Resources Loader

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,7 +4,32 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const path = require('path')
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/scss/globals/_index.scss'),
+      ],
+    })
+}
+
 module.exports = {
   siteName: 'Gridsome',
   plugins: [],
+  chainWebpack (config) {
+    // Load variables for all vue-files
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+
+    types.forEach(type => {
+      addStyleResource(config.module.rule('sass').oneOf(type))
+    })
+
+    // or if you use scss
+    types.forEach(type => {
+      addStyleResource(config.module.rule('scss').oneOf(type))
+    })
+  },
 }
