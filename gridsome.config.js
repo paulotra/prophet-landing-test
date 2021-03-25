dede4f496e2385d590a9931fb1f709bb59c1618d
paulotra <trajanopaulo10@gmail.com>
2021-03-25 15:39:09 +0800

    Add PurgeCSS and Tailwind

--- gridsome.config.js
+++ gridsome.config.js
@@ -16,6 +16,15 @@ function addStyleResource (rule) {
     })
 }
 
+const tailwind = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+const postcssPlugins = [
+  tailwind(),
+]
+
+if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss(require('./purgecss.config.js')))
+
 module.exports = {
   siteName: 'Gridsome',
   plugins: [],
@@ -31,5 +40,12 @@ module.exports = {
     types.forEach(type => {
       addStyleResource(config.module.rule('scss').oneOf(type))
     })
+  },
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: postcssPlugins,
+      },
+    },
   },
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {},
+    colors: {
+      black: '#000000',
+      white: '#FFFFFF',
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
