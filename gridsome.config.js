78998cac637ab728302f00cdc6cf22cc70c66210
paulotra <trajanopaulo10@gmail.com>
2021-03-31 14:24:16 +0800

    Update Tailwind Config

--- gridsome.config.js
+++ gridsome.config.js
@@ -11,7 +11,7 @@ function addStyleResource (rule) {
     .loader('style-resources-loader')
     .options({
       patterns: [
-        path.resolve(__dirname, './src/assets/scss/globals/_index.scss'),
+        path.resolve(__dirname, './src/assets/scss/globals/_.scss'),
       ],
     })
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,66 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
-      black: '#000000',
+      transparent: 'transparent',
+      black: '#111111',
       white: '#FFFFFF',
+      grey: {
+        100: '#959DA6',
+        200: '#E1E9F2',
+        300: '#F5F6F8',
+        400: '#F5F6F8',
+        500: '#C0C6CC',
+        600: '#FAFAFB',
+        700: '#FAFAFA',
+      },
+      navy: {
+        100: '#1B2242',
+        200: '#343B5E',
+      },
+      teal: {
+        100: '#55E8B9',
+      },
+      red: {
+        100: '#FF677A',
+      },
+      blue: {
+        100: '#50B4E3',
+      },
+      purple: {
+        100: '#8B52F5',
+      },
+      yellow: {
+        100: '#FFBB4A',
+      },
+    },
+    textSizes: {
+      '2xs': '.625em', // 10px
+      xs: '.75em', // 12px
+      sm: '.875em', // 14px
+      base: '1em', // 16px
+      lg: '1.125em', // 18px
+      xl: '1.25em', // 20px
+      '2xl': '1.5em', // 24px
+      '3xl': '1.75em' // 28px
+    },
+    tracking: {
+      normal: '1px'
+    },
+    spacing: {
+      '0': '0',
+      '1': '1px',
+      '2': '2px',
+      '3': '3px',
+      '5': '5px',
+      '10': '10px',
+      '15': '15px',
+      '20': '20px',
+      '25': '25px',
+      '30': '30px',
+      '35': '35px',
+      '40': '40px',
+      '45': '45px',
+      '50': '50px',
     }
   },
   variants: {
